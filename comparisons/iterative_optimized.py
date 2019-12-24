c7d34aefb7701787d7db632377a6c98fcf05d843
RyanStonebraker <ryanastonebraker@gmail.com>
2019-12-24 12:56:25 -0900

    Reduces conflict resolution to one loop in iterative comparison

--- comparisons/iterative_optimized.py
+++ comparisons/iterative_optimized.py
@@ -12,20 +12,25 @@ def getMainPermutations(intervals, key):
     conflicts = False
     new_intervals = []
     intervals = sorted(intervals, key=itemgetter("start", "finish"))
+    compare_search = 0
     for start_interval in intervals:
         min_start = start_interval["start"]
         max_list = [start_interval["finish"]]
-        for compare_interval in intervals:
-            if compare_interval["finish"] <= start_interval["start"] or start_interval == compare_interval:
+        for compare_i, compare_interval in enumerate(intervals[compare_search:]):
+            if compare_interval["finish"] <= start_interval["start"]:
+                if start_interval != compare_interval:
+                    compare_search = compare_i
                 continue
             elif compare_interval["start"] > start_interval["finish"]:
                 break
             conflicts = True
 
             if start_interval["start"] < compare_interval["start"] < start_interval["finish"]:
-                max_list.append(compare_interval["start"])
+                if compare_interval["start"] not in max_list:
+                    max_list.append(compare_interval["start"])
             if start_interval["start"] < compare_interval["finish"] < start_interval["finish"]:
-                max_list.append(compare_interval["finish"])
+                if compare_interval["finish"] not in max_list:
+                    max_list.append(compare_interval["finish"])
 
         max_list = sorted(max_list)
         for end in max_list:
@@ -38,33 +43,47 @@ def getMainPermutations(intervals, key):
     return conflicts, new_intervals
 
 
+# def resolveConflicts(intervals, key):
+#     resolved_intervals = []
+#     skip = {}
+#     search_start = 0
+#     for start_interval in intervals:
+#         if (start_interval["start"], start_interval["finish"]) in skip:
+#             continue
+
+#         conflict = False
+#         for j, compare_interval in enumerate(intervals[search_start:]):
+#             if compare_interval["finish"] < start_interval["start"]:
+#                 search_start = j
+#                 continue
+
+#             if start_interval["start"] == compare_interval["start"] and start_interval["finish"] > compare_interval["finish"]:
+#                 conflict = True
+#                 break
+#             if start_interval["finish"] < compare_interval["start"]:
+#                 break
+
+#             elif (start_interval["start"], start_interval["finish"]) == (compare_interval["start"], compare_interval["finish"]):
+#                 start_interval[key] = start_interval[key].union(compare_interval[key])
+#                 skip[(start_interval["start"], start_interval["finish"])] = True
+
+#         if not conflict:
+#             resolved_intervals.append(start_interval)
+#     return resolved_intervals
+
+
 def resolveConflicts(intervals, key):
     resolved_intervals = []
-    skip = {}
-    search_start = 0
-    for start_interval in intervals:
-        if (start_interval["start"], start_interval["finish"]) in skip:
-            continue
-
-        conflict = False
-        for j, compare_interval in enumerate(intervals[search_start:]):
-            if compare_interval["finish"] < start_interval["start"]:
-                search_start = j
-                continue
-
-            if start_interval["start"] == compare_interval["start"] and start_interval["finish"] > compare_interval["finish"]:
-                conflict = True
-                break
-            if start_interval["finish"] < compare_interval["start"]:
-                break
-
-            elif (start_interval["start"], start_interval["finish"]) == (compare_interval["start"], compare_interval["finish"]):
-                start_interval[key] = start_interval[key].union(compare_interval[key])
-                skip[(start_interval["start"], start_interval["finish"])] = True
-
-        if not conflict:
-            resolved_intervals.append(start_interval)
+    current_interval = intervals[0]
+    for interval in intervals[1:]:
+        if interval["start"] > current_interval["start"]:
+            resolved_intervals.append(current_interval)
+            current_interval = interval
+        else:
+            current_interval[key] = current_interval[key].union(interval[key])
+    resolved_intervals.append(current_interval)
     return resolved_intervals
+
 
 class Merge:
     @staticmethod
